6d2c0b34adfb1b3b78f14cf9d7c30438857431f5
joasmund <jorgen.asmundvaag@nmbu.no>
2023-08-28 20:57:50 +0200

    First commit

---
+++ Python_start.py
@@ -0,0 +1,9 @@
+#input name 
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+#input age
+age = input("How old are you? ")
+print("You are " + age + " years old!")
+#input city
+city = input("Where do you live? ")
+print("You live in " + city + "!")
